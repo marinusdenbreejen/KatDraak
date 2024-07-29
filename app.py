ac833e3879c5c23bcf5396bf2c11d31ac0a31575
marinusdenbreejen <71934303+marinusdenbreejen@users.noreply.github.com>
2024-07-29 12:22:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='gevent')
 
 counter = {'count': 0}
 
