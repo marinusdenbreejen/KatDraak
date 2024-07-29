2a6ea85e0ab39ee8d5ccf43dd54cfd9b1baba95c
marinusdenbreejen <71934303+marinusdenbreejen@users.noreply.github.com>
2024-07-29 12:10:35 +0200

    manual add app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+counter = {'count': 0}
+
+@app.route('/')
+def hello_world():
+    return jsonify(message='Hello World', counter=counter['count'])
+
+@socketio.on('increment')
+def handle_increment():
+    counter['count'] += 1
+    emit('counter_update', {'counter': counter['count']}, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
